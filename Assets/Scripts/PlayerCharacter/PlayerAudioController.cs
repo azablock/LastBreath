0e18559da8046548f4151393f2c605bcdfe974d9
azab9 <azab91@gmail.com>
2018-03-12 22:46:25 +0100

    add footstep sound

---
+++ Assets/Scripts/PlayerCharacter/PlayerAudioController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerAudioController : MonoBehaviour {
+
+  //todo refactor
+  private MovementController _movementController;
+  private float _nextStepTime;
+  private float _interval;
+
+  private void Start() {
+    _movementController = GetComponent<MovementController>();
+    _nextStepTime = 0.0f;
+    _interval = 0.4f;
+  }
+
+  private void Update() {
+    if (Time.time > _nextStepTime && _movementController.MovementDelta.magnitude > 0.0f) {
+      _nextStepTime += _interval;
+      AkSoundEngine.PostEvent("Play_Footstep", gameObject);
+    }
+  }
+}
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEP
+        {
+            static const AkUniqueID GROUP = 1866025847U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DEFAULT_FOOTSTEP = 2223364641U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEP
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
