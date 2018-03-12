24fbe0874e3ec39b2ffe08a306a57fe43c49768e
azab9 <azab91@gmail.com>
2018-03-12 19:30:15 +0100

    add player character movement, add player character animator controller add Scripts/ subfolders

---
+++ Assets/Scripts/PlayerCharacter/InputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+//[RequireComponent(typeof(MovementController))]
+public class InputController : MonoBehaviour {
+
+  private MovementController _movementController;
+
+  private void Awake() {
+    _movementController = GetComponent<MovementController>();
+  }
+
+  private void Update() {
+    ProcessMovementInputEvents();
+  }
+
+  private void ProcessMovementInputEvents() {
+    var horizontalInputValue = Input.GetAxis("Horizontal");
+    _movementController.MovementDelta = new Vector2(horizontalInputValue, 0.0f); //todo refactor?
+  }
+}
